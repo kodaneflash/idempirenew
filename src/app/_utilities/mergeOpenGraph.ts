f6aa9821cf513f1ae908f3d900bdba7c80696464
kodaneflash <jamesonmarcher@gmail.com>
2024-04-12 00:20:04 -0400

    ]#	new file:   public/og-image.png

--- src/app/_utilities/mergeOpenGraph.ts
+++ src/app/_utilities/mergeOpenGraph.ts
@@ -2,12 +2,13 @@ import type { Metadata } from 'next'
 
 const defaultOpenGraph: Metadata['openGraph'] = {
   type: 'website',
-  siteName: 'Payload E-Commerce Template',
-  title: 'Payload E-Commerce Template',
-  description: 'An open-source e-commerce store built with Payload and Next.js.',
+  siteName: 'IDEmpire - #1 Trusted Scannable Fake ID Vendor',
+  title: 'IDEmpire',
+  description:
+    'IDEmpire is the #1 trusted fake ID vendor that offers scannable barcodes, advanced holograms, microprint, laser engraving & more. We offer fast shipping, fair prices & quality customer support.',
   images: [
     {
-      url: 'https://payloadcms.com/images/og-image.jpg',
+      url: 'https://idempire.us/og-image.jpg',
     },
   ],
 }
